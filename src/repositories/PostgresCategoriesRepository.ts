a61a271c0a9be01a8b39ab60b16da418561f9a2d
Ericlys Moreira <ericlysm@gmail.com>
2022-08-24 07:39:06 -0300

    feat: Using the Liskov Substitution Principle

---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,21 @@
+import { Category } from "../model/Category";
+import {
+  ICategoriesRespository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class PostgresCategoriesRepository implements ICategoriesRespository {
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+  list(): Category[] {
+    return null;
+  }
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+    return null;
+  }
+}
+
+export { PostgresCategoriesRepository };
