43775b63b842bd251188e3972f878de0880f4a3f
Ericlys Moreira <ericlysm@gmail.com>
2022-08-25 23:22:57 -0300

    fix: specification use case

---
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -0,0 +1,13 @@
+import { SpecificationRepository } from "../../repositories/implementations/SpecificationRepository";
+import { CreateSpecificationController } from "./CreateSpecificationController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specificationRepository = new SpecificationRepository();
+const specificationUseCase = new CreateSpecificationUseCase(
+  specificationRepository
+);
+const createSpecificationController = new CreateSpecificationController(
+  specificationUseCase
+);
+
+export { createSpecificationController };
