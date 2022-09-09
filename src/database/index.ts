837a63c1d6ce134c90c2dc70b737388b6fd44b35
Ericlys Moreira <ericlysm@gmail.com>
2022-09-09 07:54:54 -0300

    feat: user module and user migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,11 +5,12 @@ import { Category } from "../modules/cars/entities/Category";
 import { Specification } from "../modules/cars/entities/Specification";
 import { CreateCategories1662575549333 } from "./migrations/1662575549333-CreateCategories";
 import { CreateSpecifications1662605988388 } from "./migrations/1662605988388-CreateSpecifications";
+import { CreateUsers1662689908852 } from "./migrations/1662689908852-CreateUsers";
 // yarn typeorm -d src/database/index.ts migration:run
 export const AppDataSource = new DataSource({
   type: "postgres",
   port: 5432,
-  host: "database",
+  host: "localhost",
   username: "docker",
   password: "ignite",
   database: "rentx",
@@ -17,6 +18,7 @@ export const AppDataSource = new DataSource({
   migrations: [
     CreateCategories1662575549333,
     CreateSpecifications1662605988388,
+    CreateUsers1662689908852,
   ],
 });
 
