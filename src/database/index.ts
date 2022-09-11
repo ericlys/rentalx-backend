4f69c19522542bf2c73ac778d81b071d07b9ffe5
Ericlys Moreira <ericlysm@gmail.com>
2022-09-11 15:42:27 -0300

    feat: improving configuration with typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,35 +1,24 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-
-import { User } from "../modules/accounts/entities/User";
-import { Category } from "../modules/cars/entities/Category";
-import { Specification } from "../modules/cars/entities/Specification";
-import { CreateCategories1662575549333 } from "./migrations/1662575549333-CreateCategories";
-import { CreateSpecifications1662605988388 } from "./migrations/1662605988388-CreateSpecifications";
-import { CreateUsers1662689908852 } from "./migrations/1662689908852-CreateUsers";
-import { AlterUserDeleteUsername1662871280861 } from "./migrations/1662871280861-AlterUserDeleteUsername";
 // yarn typeorm -d src/database/index.ts migration:run
+// yarn typeorm migration:create src/database/migrations/{name}
 export const dataSource = new DataSource({
   type: "postgres",
   port: 5432,
-  host: "database_rentalx",
+  host: "localhost",
   username: "docker",
   password: "ignite",
   database: "rentx",
-  entities: [Category, Specification, User],
-  migrations: [
-    CreateCategories1662575549333,
-    CreateSpecifications1662605988388,
-    CreateUsers1662689908852,
-    AlterUserDeleteUsername1662871280861,
-  ],
+  entities: ["src/modules/**/entities/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
 });
 
-dataSource
-  .initialize()
-  .then(async () => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
+export function createConnection(
+  host = "database_rentalx"
+): Promise<DataSource> {
+  return dataSource
+    .setOptions({
+      host,
+    })
+    .initialize();
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-
-import "./database";
+import "reflect-metadata";
 
 import "./shared/container";
 
+import { createConnection } from "./database";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+
+createConnection();
 
 const app = express();
 
