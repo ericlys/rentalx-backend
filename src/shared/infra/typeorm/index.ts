d152b02b1f5c66f8f3469c7f086c98812cd78f39
Ericlys Moreira <ericlysm@gmail.com>
2022-09-25 09:30:28 -0300

    fix: changing migrations imports

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 // yarn typeorm -d src/database/index.ts migration:run
 // yarn typeorm migration:create src/database/migrations/{name}
+// yarn typeorm migration:create src/shared/infra/typeorm/migrations/CreateCars
 export const dataSource = new DataSource({
   type: "postgres",
   port: 5432,
@@ -10,7 +11,7 @@ export const dataSource = new DataSource({
   password: "ignite",
   database: "rentx",
   entities: ["src/modules/**/entities/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
+  migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
 });
 
 export function createConnection(
