4a75867241d3814e2fb0999779f61e044d5ff444
Ericlys Moreira <ericlysm@gmail.com>
2022-10-06 07:04:58 -0300

    feat: integration test for category listing

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -23,7 +23,7 @@ class CreateCategoryUseCase {
     if (categoryAlreadyExists) {
       throw new AppError("Category already exists!");
     }
-    this.categoriesRepository.create({ name, description });
+    await this.categoriesRepository.create({ name, description });
   }
 }
 
