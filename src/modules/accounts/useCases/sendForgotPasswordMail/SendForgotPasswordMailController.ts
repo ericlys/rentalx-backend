d919cddfd84b53e01f17b8bfb812564810e5f023
Ericlys Moreira <ericlysm@gmail.com>
2022-10-10 07:38:21 -0300

    feat: email provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -3,7 +3,7 @@ import { container } from "tsyringe";
 
 import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
 
-class SendForgotPasswordController {
+class SendForgotPasswordMailController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
     const sendForgotPasswordMailUseCase = container.resolve(
@@ -16,4 +16,4 @@ class SendForgotPasswordController {
   }
 }
 
-export { SendForgotPasswordController };
+export { SendForgotPasswordMailController };
