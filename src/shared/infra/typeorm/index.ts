5c3b4deb33c72ad67899a41168d1d76b79a1eb1e
Ericlys Moreira <ericlysm@gmail.com>
2022-10-19 06:50:53 -0300

    fix: Database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,18 +6,20 @@ import { DataSource } from "typeorm";
 // yarn typeorm migration:create src/shared/infra/typeorm/migrations/CreateCars
 export const dataSource = new DataSource({
   type: "postgres",
-  port: 5432,
-  host: "localhost",
-  username: "docker",
-  password: "ignite",
-  database: process.env.NODE_ENV === "test" ? "rentx_test" : "rentx",
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  // host: process.env.NODE_ENV === "test" ? "localhost" : "database_rentalx",
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === "test"
+      ? process.env.DB_DATABASE_TEST
+      : process.env.DB_DATABASE,
   entities: ["src/modules/**/entities/*.ts"],
   migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
 });
 
-export async function createConnection(
-  host = process.env.NODE_ENV === "test" ? "localhost" : "database_rentalx"
-): Promise<DataSource> {
+export async function createConnection(host): Promise<DataSource> {
   return dataSource
     .setOptions({
       host,
