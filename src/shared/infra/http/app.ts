236d9a4a83cca94a29132f2622dcf1afb210c24d
Ericlys Moreira <ericlysm@gmail.com>
2022-10-30 10:12:07 -0300

    feat: sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,8 @@ import "reflect-metadata";
 
 import "@shared/container";
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import { createConnection } from "@shared/infra/typeorm";
@@ -20,6 +22,18 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -30,6 +44,8 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
